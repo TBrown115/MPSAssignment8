c7c5c497f547179a16f0d4a04e9394a6c5c7fd72
TBrown115 <70948046+TBrown115@users.noreply.github.com>
2020-12-08 20:36:20 +0200

    first commit

---
+++ People/App.xaml.cs
@@ -0,0 +1,43 @@
+using People.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace People
+{
+    public partial class App : Application
+    {
+        string dbPath => FileAccessHelper.GetLocalFilePath("tshirt.db3");
+
+        public static PersonRepository PersonRepo { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            PersonRepo = new PersonRepository(dbPath);
+            var navPage = new NavigationPage(new TShirtPage());
+            navPage.BackgroundColor = Color.FromHex("643B10");
+            MainPage = navPage;
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ People/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+
+namespace People.Models
+{
+    [Table("people")]
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(250), Unique]
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string TShirt_Size { get; set; }
+        public string DateOfOrder { get; set; }
+        public string TShirt_Color { get; set; }
+        public string ShippingAddress { get; set; }
+    }
+}
